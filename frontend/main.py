327983554fa12f96b2f278535e973b01bd61ff5c
Charysse <charysse.redwood@gmail.com>
2021-03-24 19:55:27 -0700

    added submit button

--- frontend/main.py
+++ frontend/main.py
@@ -5,23 +5,24 @@ from backend.main import Test
 # defines an h1 header
 st.title("is this a joke")
 
-# displays a file uploader widget
-text = st.text_input('Input your sentence here:') 
+# displays a text field
+text = st.text_area('max. 500 words') 
 
 # displays a button
-if text:
-    test = Test(text=[text])
-    res = requests.post(f"http://backend:8080/test", test=test)
-    st.write(f"model response: {res.result[0]}, probability: {res.softmax[0]}")
-    st.write(f"does the model's response of {res.result[0]} seem correct?")
-    yes = st.button("Yes")
-    no = st.button("No")
-    if yes:
-        st.write(f"Thank you for your feedback!")
-        res.ground_truth = [1]
-        requests.post(f"http://backend:8080/test", test=res)
-    elif no:
-        res.ground_truth = [0]
-        requests.post(f"http://backend:8080/test", test=res)
-        st.write(f"Thank you for your feedback!")
+if st.button('submit'):
+    if text is not None:
+        test = Test(text=[text])
+        res = requests.post(f"http://backend:8080/test", test=test)
+        st.write(f"model response: {res.result[0]}, probability: {res.softmax[0]}")
+        st.write(f"does the model's response of {res.result[0]} seem correct?")
+        yes = st.button("Yes")
+        no = st.button("No")
+        if yes:
+            st.write(f"Thank you for your feedback!")
+            res.ground_truth = [1]
+            requests.post(f"http://backend:8080/test", test=res)
+        elif no:
+            res.ground_truth = [0]
+            requests.post(f"http://backend:8080/test", test=res)
+            st.write(f"Thank you for your feedback!")
     
