e7c4442578e22cff65bab7b56701940c6c428824
Charysse <charysse.redwood@gmail.com>
2021-03-24 18:33:02 -0700

    chanegd back

--- frontend/main.py
+++ frontend/main.py
@@ -1,8 +1,6 @@
 # frontend/main.py
 import requests
 import streamlit as st
-from backend.main import Test
-
 
 # defines an h1 header
 st.title("is this a joke")
@@ -12,8 +10,7 @@ text = st.text_input('Input your sentence here:')
 
 # displays a button
 if text:
-    test = Test(text=[text])
-    res = requests.post(f"http://backend:8080/test", test=test)
+    res = requests.post(f"http://backend:8080/test", test.text=[text])
     st.write(f"model response: {res.result[0]}, probability: {res.softmax[0]}")
     st.write(f"does the model's response of {res.result[0]} seem correct?")
     yes = st.button("Yes")
