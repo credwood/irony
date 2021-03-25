b2c249ceb016604edc1474ad432f9cb8d4b28f57
Charysse <charysse.redwood@gmail.com>
2021-03-25 01:48:52 -0700

    bugs

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import FileResponse, RedirectResponse
+from fastapi.responses import FileResponse, RedirectResponse, HTMLResponse
 from typing import Optional, List
 from pydantic import BaseModel
 from inference import predict
@@ -34,6 +34,21 @@ def recognize_enteties(test: Test):
         test.result = pred[1]
         test.softmax = pred[2]
         return {"message": test}
+
+@app.post('/about/')
+def about():
+    html_content = """
+    <html>
+        <head>
+            <title>about</title>
+        </head>
+        <body>
+            <p>Some jokes are defined by their context, some jokes are defined by their form. Many are a mix of the two.</p>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080)
--- frontend/main.py
+++ frontend/main.py
@@ -5,6 +5,9 @@ from backend.main import Test
 # defines an h1 header
 st.title("is this a joke")
 
+#link to about page
+st.write("[about](http://backend:8080/about)")
+
 # displays a text field
 text = st.text_area('max. 500 words') 
 
@@ -13,17 +16,17 @@ if st.button('submit'):
     if text is not None:
         test = Test(text=[text])
         res = requests.post("http://backend:8080/test", data=test)
-        st.write(f"model response: {res.result[0]}, probability: {res.softmax[0]}")
-        st.write(f"does the model's response of {res.result[0]} seem correct?")
+        st.write(f"model response: {res.message.result[0]}, probability: {res.message.softmax[0]}")
+        st.write(f"does the model's response of {res.message.result[0]} seem correct?")
         yes = st.button("Yes")
         no = st.button("No")
         if yes:
             st.write("Thank you for your feedback!")
-            res.ground_truth = [1]
-            requests.post("http://backend:8080/test", data=res)
+            res.message.ground_truth = [1]
+            requests.post("http://backend:8080/test", data=res.message)
         elif no:
             st.write("Thank you for your feedback!")
-            res.ground_truth = [0]
-            requests.post("http://backend:8080/test", data=res)
+            res.message.ground_truth = [0]
+            requests.post("http://backend:8080/test", data=res.message)
             
     
