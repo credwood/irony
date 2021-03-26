2650f7a7808c056af7603ae735f1eb5aef3b7d3d
Charysse <charysse.redwood@gmail.com>
2021-03-26 03:33:14 -0700

    troubleshoting

--- frontend/main.py
+++ frontend/main.py
@@ -1,7 +1,6 @@
 # frontend/main.py
 import requests
 import streamlit as st
-from backend.main import Test
 # defines an h1 header
 st.title("is this a joke")
 
@@ -14,19 +13,27 @@ text = st.text_area('max. 500 words')
 # displays a button
 if st.button('submit'):
     if text is not None:
-        test = Test(text=[text])
-        res = requests.post("http://backend:8080/test", data=test)
-        st.write(f"model response: {res.message.result[0]}, probability: {res.message.softmax[0]}")
-        st.write(f"does the model's response of {res.message.result[0]} seem correct?")
+        res = requests.post("http://backend:8080/test", json={"message":
+                                                                    {"id": 0,
+                                                                    "text": [text],
+                                                                    "result":[],
+                                                                    "softmax":[],
+                                                                    "ground_truth":[5]
+                                                                    }
+                                                            }
+                            )
+        res = res.json()
+        st.write(f"model response: {res.get("result")[0]}, probability: {res.get("softmax")[0]}")
+        st.write(f"does the model's response of {res.get("result")[0]} seem correct?")
         yes = st.button("Yes")
         no = st.button("No")
         if yes:
             st.write("Thank you for your feedback!")
-            res.message.ground_truth = [1]
-            requests.post("http://backend:8080/test", data=res.message)
+            res.ground_truth = [1]
+            requests.post("http://backend:8080/test", json=res)
         elif no:
             st.write("Thank you for your feedback!")
-            res.message.ground_truth = [0]
-            requests.post("http://backend:8080/test", data=res.message)
+            res.ground_truth = [0]
+            requests.post("http://backend:8080/test", json=res)
             
     
